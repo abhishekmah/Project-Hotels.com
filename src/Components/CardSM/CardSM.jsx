b515e784c3db07581ed44af2a1d83b3efc495f24
Ransingh88 <rajaransingh@gmail.com>
2021-08-25 21:01:04 +0530

    small card code updated

--- src/Components/CardSM/CardSM.jsx
+++ src/Components/CardSM/CardSM.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Card = styled.div`
-margin: 0.5rem 0.25rem;
+margin: 0.8rem 0.25rem 0.6rem 0.25rem;
 padding: 0.25rem;
 img{
     width: 180px;
@@ -14,11 +14,10 @@ p{
 }
 `
 
-const CardSM = () => {
+const CardSM = ({children}) => {
     return (
         <Card>
-            <img src="https://mediaim.expedia.com/destination/1/b29d96f82af39a8a799e486a4f5be226.jpg?impolicy=fcrop&w=179&h=101&q=high" alt="" />
-            <p>Puri</p>
+            {children}
         </Card>
     )
 }

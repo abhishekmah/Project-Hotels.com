a261f50e75ee8bbbec87c0a8696b7306d62d9c56
Ransingh88 <rajaransingh@gmail.com>
2021-08-25 20:27:59 +0530

    small card applied in home page and styled home page components

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Home/>
       {/* <LastMinDeals /> */}
+      
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CardSM } from '../../Components/CardSM/CardSM'
 import { CovidAd } from '../../Components/CovidAd/CovidAd'
 import { HRLine } from '../../Components/HorizontalLine/HRLine'
 import {Navbar} from '../../Components/Navbar/Navbar'
@@ -11,13 +12,25 @@ const Home = () => {
             <HRLine />
             <Navbar />
             <div className={styles.home__backgroundImage}>
-            <img src="https://a.cdn-hotels.com/cos/heroimage/homepage/INDIA.jpg?impolicy=fcrop&amp;w=1900&amp;h=425&amp;q=high" alt="" />
+            <img src="./images/backgroundImg.png" alt="" />
                 <div className={styles.searchCard}>
                     <p>Where to?</p>
             </div>
             </div>
-
+            <div className={styles.home__body}>
+                <div className={styles.home__Card}> 
+                <CardSM/>
+                <CardSM/>
+                <CardSM/>
+                <CardSM/>
+                <CardSM/>
+                <CardSM/>
+                    </div>
+            </div>
         </div>
+        
+
+
     )
 }
 

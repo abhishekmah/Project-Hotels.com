ec0d9053600243fa2c31e50b2b5c0b360c985dd6
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-08-25 11:42:54 +0530

    hotel 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import LastMinDeals from "./Pages/LastMinDeals"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <img src="./images/hotels-logo.png" alt="" /> */}
+      <LastMinDeals />
+    </div>
+  );
+}
+
+export default App;

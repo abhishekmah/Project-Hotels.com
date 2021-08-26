f4c1a9c6deb68de3b20e074d8042bb3aafe6cac6
Ransingh88 <rajaransingh@gmail.com>
2021-08-26 23:57:57 +0530

    navbar icons updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { IoMdArrowDropdown } from 'react-icons/io';
 
 const Navbar = () => {
     return (
@@ -8,11 +10,11 @@ const Navbar = () => {
             <div className={styles.nav__navbar}>
             <img src="./images/hotels-logo1.png" alt="Hotels Logo"  />
                 <div className={styles.nav__menus}>
-                    <p>Country</p>
+                    <p> <img src="./images/india.png" alt="india icon"  width="15px"/> <IoMdArrowDropdown/></p>
                     <p>INR</p>
                     <p>Help</p>
-                    <p>Your Bookings</p>
-                    <p>Signin</p>
+                    <p><FaRegCalendarAlt/> Your Bookings</p>
+                    <p>Sign in</p>
             </div>
             </div>
             <div className={styles.nav__extrasLink}>

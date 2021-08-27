bbcf18832fc50d88cd3066fd67147c4f09c58b6f
Ransingh88 <rajaransingh@gmail.com>
2021-08-27 21:24:11 +0530

    added Router components and set route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { Home } from './Pages/Home/Home';
 import './App.css';
 import LastMinDeals from "./Pages/LastDeals/LastMinDeals"
 import { Signin } from './Pages/Auth/Signin';
+import { Routes } from './Routes/Route';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-     
-      {/* <LastMinDeals /> */}
-      <Signin/>
+      
+      <Routes/>
       
     </div>
   );

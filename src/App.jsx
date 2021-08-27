f520eb72a658a5941d4031aafb6df84d58d8699b
Ransingh88 <rajaransingh@gmail.com>
2021-08-27 21:09:48 +0530

    sign in component aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Home } from './Pages/Home/Home';
 import './App.css';
 import LastMinDeals from "./Pages/LastDeals/LastMinDeals"
+import { Signin } from './Pages/Auth/Signin';
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
      
       {/* <LastMinDeals /> */}
+      <Signin/>
       
     </div>
   );

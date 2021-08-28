65a59873e626bba648019fcb75748fa7b85723e4
Abhishek Mahato <mahatoabhishek123@gmail.com>
2021-08-28 10:39:39 +0530

    linked DealsPage

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch,Route} from 'react-router-dom'
 import { Signin } from '../Pages/Auth/Signin'
 import { Home } from '../Pages/Home/Home'
+import LastMinDeals from '../Pages/LastDeals/LastMinDeals'
 
 const Routes = () => {
     return (
@@ -10,6 +11,7 @@ const Routes = () => {
                 <Route exact path="/" > <Home/> </Route>
                 <Route path="/signin" > <Signin/> </Route>
                 <Route path="/signup" > </Route>
+                <Route path="/lastMinDeals" > <LastMinDeals/></Route>
             </Switch>
             
         </div>

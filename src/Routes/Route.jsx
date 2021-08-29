f90d8fb2e1f79118f4c622234218503b573c90e2
Ransingh88 <rajaransingh@gmail.com>
2021-08-29 16:09:47 +0530

    some code update

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -4,6 +4,7 @@ import { Signin } from '../Pages/Auth/Signin'
 import { Signup } from '../Pages/Auth/Signup'
 import { Home } from '../Pages/Home/Home'
 import LastMinDeals from '../Pages/LastDeals/LastMinDeals'
+import {Payment} from '../Pages/Payment/Payment'
 
 const Routes = () => {
     return (
@@ -13,8 +14,8 @@ const Routes = () => {
                 <Route path="/signin" > <Signin/> </Route>
 
                 <Route path="/signup" > <Signup/> </Route>
+                <Route path="/payment" > <Payment/> </Route>
 
-                <Route path="/signup" > </Route>
                 <Route path="/lastMinDeals" > <LastMinDeals/></Route>
 
             </Switch>

b5159c5ac820c36a0e197ec69ac599c1c633e732
Nitish <ozeus459@gmail.com>
2021-08-29 10:24:07 +0530

    files

---
+++ src/Pages/Perfect Propts/Props.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export default function Props() {
+    return (
+        <div className="body">
+            <section class="_34UURu">
+                <div class="_1quDY2">
+                  <div class="_2h6Jhd">
+                    <h1>explora Patagonia</h1>
+                    <button><span class="_2dOcxA">5-star</span></button>
+                  </div>
+                  <p>
+                    All-inclusive hotel with indoor pool, near Torres del Paine
+                    National Park
+                  </p>
+                  <div class="_36kKl_">
+                    <ul class="FWXvlZ LIBjVd uCBf52">
+                      <li><span>Free WiFi in lobby and free parking</span></li>
+                      <li class="_2BKDS4">
+                        <span
+                          ><span class="_3zLN_v _2VINEh b3TRQj"
+                            ><span
+                              role="button"
+                              class="k3LKyj"
+                              aria-expanded="false"
+                              tabindex="0"
+                              ><span>Collect stamps</span></span
+                            ><span
+                              class="_23w5Ct _2mMB8-"
+                              aria-live="polite"
+                              role="tooltip"
+                              data-content="tooltip"
+                              aria-hidden="true"
+                              ><span
+                                >You can collect Hotels.com™ Rewards stamps
+                                here</span
+                              ></span
+                            >
+                          </span>
+                        </span>
+                      </li>
+                      <li class="_1maYF9">
+                        <span
+                          ><span class="x6ZO-g _1ETyZf x30YJE"
+                            ><span
+                              class="_3C7CXe"
+                              aria-expanded="false"
+                              tabindex="0"
+                              role="button"
+                              >Price Guarantee</span
+                            ><span
+                              class="mB2Uy6 _3Xu82n"
+                              role="tooltip"
+                              aria-hidden="true"
+                              ><article
+                                class="B8D2gM _1TsFNW _2L62Ud _1b9m_O _13lxWh"
+                              >
+                                <div class="_3MItkq">
+                                  <h4 class="_2gYmT9">Found a better price?</h4>
+                                  <p class="_1_DtK_">
+                                    Let us know and we’ll match it.<a
+                                      href="https://in.hotels.com/customer_care/pillar/price_guarantee.html"
+                                      class="_2fGjNo"
+                                      target="_blank"
+                                      rel="noopener noreferrer"
+                                      >More info<span class="is-visually-hidden"
+                                        >Opens in a new window</span
+                                      ></a
+                                    >
+                                  </p>
+                                </div>
+                              </article>
+                            </span>
+                          </span>
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <div class="_3rmeaZ">
+                    <div class="_3cxHmh"></div>
+                  </div>
+                  <div class="_1vI19r">
+                    <button
+                      class="_3pFoIe _1nzQQs"
+                      aria-label="Select dates"
+                      type="button"
+                    >
+                      Select dates
+                    </button>
+                  </div>
+                </div>
+              </section>
+        </div>
+    )
+}
